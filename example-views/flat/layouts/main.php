0097240d08e478c577bfda17315d29cbc6c19b55
Pedro Plowman <pedrofp@me.com>
2015-05-18 02:22:46 +1000

    see how this goes

---
+++ example-views/flat/layouts/main.php
@@ -0,0 +1,17 @@
+<?php
+use yii\helpers\Html;
+use p2made\theme\Flat\widgets\Alert;
+
+/* @var $this \yii\web\View */
+/* @var $content string */
+
+?>
+<?php $this->beginPage() ?>
+<!DOCTYPE html>
+<html lang="<?= Yii::$app->language ?>">
+<body>
+	<?php $this->beginBody() ?>
+	<?php $this->endBody() ?>
+</body>
+</html>
+<?php $this->endPage() ?>
