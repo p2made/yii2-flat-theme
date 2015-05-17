47965629b26997eadc81bde7ef811f0f7309cc9b
Pedro Plowman <pedrofp@me.com>
2015-05-18 01:43:49 +1000

    debug 1

--- example-views/flat/layouts/main_.php
+++ example-views/flat/layouts/main_.php
@@ -9,8 +9,19 @@ use p2made\theme\Flat\widgets\Alert;
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>">
+	<?= $this->render('html-header.php', []) ?>
 <body>
 	<?php $this->beginBody() ?>
+	<?= $this->render('header.php', []) ?>
+
+	<?php if (Yii::$app->controller->action->id !== 'index') {
+		$this->render('page-title.php', []);
+	} ?>
+
+	<?= Alert::widget() ?>
+	<?= $content ?>
+
+	<?= $this->render('footer.php', []) ?>
 	<?php $this->endBody() ?>
 </body>
 </html>
